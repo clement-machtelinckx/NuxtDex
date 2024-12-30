518627878ff11d17505f2b0279d40529e2be3933
cmachtelinckx <cmachtelinckx@groupempsa.com>
2024-12-30 11:34:05 +0100

    refactor Header and PokeResume components: enhance layout, add back navigation, and improve responsiveness

--- components/PokeResume.vue
+++ components/PokeResume.vue
@@ -30,35 +30,25 @@ const hiddenMoves = computed(() => pokemon.value?.moves.slice(4) || [])
   <div v-if="isLoading">Chargement...</div>
   <div v-else-if="error">{{ error }}</div>
   <div v-else-if="pokemon">
-    <v-card class="mt-10 border-md elevation-10" text="">
-      <v-card-title><strong>{{ pokemon.name }}</strong></v-card-title>
-      <v-card-item class="image-container">
-        <div>
-          <img :src="pokemon.sprites.other.home.front_default" alt="Image par défaut du Pokémon" />
-        </div>
-        <div>
-          <img :src="pokemon.sprites.other.home.front_shiny" alt="Image shiny du Pokémon" />
-        </div>
-      </v-card-item>
-      <v-card-text><strong>Type:</strong> {{ pokemon.types.map(type => type.type.name).join(', ') }}</v-card-text>
-      <v-card-text><strong>Poids:</strong> {{ pokemon.weight }} hectogrammes</v-card-text>
-      <v-card-text><strong>Taille:</strong> {{ pokemon.height }} décimètres</v-card-text>
-      <v-card-text><strong>Numéro du Pokémon:</strong> #{{ pokemon.id }}</v-card-text>
-      
-      <v-card-text><strong>Attaques:</strong></v-card-text>
-      <ul class="list-group">
-        <li v-for="move in visibleMoves" :key="move.move.name">
-          {{ move.move.name }}
-          <span v-if="move.version_group_details.length > 0">
-            lvl {{ move.version_group_details[0].level_learned_at }}
-          </span>
-          <span v-else>
-            (niveau inconnu)
-          </span>
-        </li>
-
-        <template v-if="hiddenMoves.length > 0">
-          <li v-show="showAllMoves" v-for="move in hiddenMoves" :key="move.move.name">
+    <v-container class="pa-4">
+      <v-card class="mx-auto" max-width="600" elevation="3">
+        <v-card-title><strong>{{ pokemon.name }}</strong></v-card-title>
+        <v-card-item class="image-container">
+          <div>
+            <img :src="pokemon.sprites.other.home.front_default" alt="Image par défaut du Pokémon" />
+          </div>
+          <div>
+            <img :src="pokemon.sprites.other.home.front_shiny" alt="Image shiny du Pokémon" />
+          </div>
+        </v-card-item>
+        <v-card-text><strong>Type:</strong> {{ pokemon.types.map(type => type.type.name).join(', ') }}</v-card-text>
+        <v-card-text><strong>Poids:</strong> {{ pokemon.weight }} hectogrammes</v-card-text>
+        <v-card-text><strong>Taille:</strong> {{ pokemon.height }} décimètres</v-card-text>
+        <v-card-text><strong>Numéro du Pokémon:</strong> #{{ pokemon.id }}</v-card-text>
+        
+        <v-card-text><strong>Attaques:</strong></v-card-text>
+        <ul class="list-group">
+          <li v-for="move in visibleMoves" :key="move.move.name">
             {{ move.move.name }}
             <span v-if="move.version_group_details.length > 0">
               lvl {{ move.version_group_details[0].level_learned_at }}
@@ -67,20 +57,38 @@ const hiddenMoves = computed(() => pokemon.value?.moves.slice(4) || [])
               (niveau inconnu)
             </span>
           </li>
-          <li>
 
-            <button @click="showAllMoves = !showAllMoves" class="toggle-button">
-              {{ showAllMoves ? 'Masquer les autres attaques' : 'Afficher plus d\'attaques' }}
-            </button>
-          </li>
-        </template>
-      </ul>
-    </v-card>
+          <template v-if="hiddenMoves.length > 0">
+            <li v-show="showAllMoves" v-for="move in hiddenMoves" :key="move.move.name">
+              {{ move.move.name }}
+              <span v-if="move.version_group_details.length > 0">
+                lvl {{ move.version_group_details[0].level_learned_at }}
+              </span>
+              <span v-else>
+                (niveau inconnu)
+              </span>
+            </li>
+            <li>
+
+              <button @click="showAllMoves = !showAllMoves" class="toggle-button">
+                {{ showAllMoves ? 'Masquer les autres attaques' : 'Afficher plus d\'attaques' }}
+              </button>
+            </li>
+          </template>
+        </ul>
+      </v-card>
+    </v-container>
   </div>
 </div>
 </template>
 
 <style scoped>
+.v-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 img {
   width: 200px;
   height: 200px;
@@ -93,7 +101,7 @@ img {
 }
 
 .v-card {
-  width: 70vh;
+  width: 70vw;
   margin: 10px;
 
 }
