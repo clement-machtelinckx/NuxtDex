67e6c955f6ac6ee7f137d24eb3f1ba40f30fb2fd
cmachtelinckx <cmachtelinckx@groupempsa.com>
2024-12-31 10:14:11 +0100

    feat: add sprite fetching and display for Pokémon in the store and UI

--- stores/usePokemonStore.js
+++ stores/usePokemonStore.js
@@ -7,6 +7,7 @@ export const usePokemonStore = defineStore('pokemonStore', () => {
   const error = ref(null)
   const next = ref(null)
   const previous = ref(null)
+  const sprites = reactive({})
 
   const fetchPokemons = async () => {
     isLoading.value = true
@@ -18,6 +19,13 @@ export const usePokemonStore = defineStore('pokemonStore', () => {
       pokemons.value = data.results
       next.value = data.next
       previous.value = data.previous
+
+      for (const pokemon of data.results) {
+        if (!sprites[pokemon.name]) {
+          await fetchSprites(pokemon.url)
+        }
+      }
+
     } catch (err) {
       error.value = err.message
     } finally {
@@ -35,6 +43,13 @@ export const usePokemonStore = defineStore('pokemonStore', () => {
       pokemons.value = data.results
       next.value = data.next
       previous.value = data.previous
+
+      for (const pokemon of data.results) {
+        if (!sprites[pokemon.name]) {
+          await fetchSprites(pokemon.url)
+        }
+      }
+
     } catch (err) {
       error.value = err.message
     } finally {
@@ -42,5 +57,21 @@ export const usePokemonStore = defineStore('pokemonStore', () => {
     }
   }
 
-  return { pokemons, isLoading, error, next, previous, fetchPokemons, fetchPokeByUrl }
+  const fetchSprites = async (url) => {
+    isLoading.value = true
+    error.value = null
+
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+
+      sprites[data.name] = data.sprites.front_default
+    } catch (err) {
+      error.value = err.message
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return { pokemons, isLoading, error, next, previous, sprites, fetchPokemons, fetchPokeByUrl }
 })
